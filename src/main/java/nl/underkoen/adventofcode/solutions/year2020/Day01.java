d8f7b2b42982d251c297e5dd8d58b33e85e84f13
UnderKoen <koenvanstaveren@hotmail.com>
2020-12-02 09:57:16 +0100

    Day 2 solution

--- src/main/java/nl/underkoen/adventofcode/solutions/year2020/Day01.java
+++ src/main/java/nl/underkoen/adventofcode/solutions/year2020/Day01.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import nl.underkoen.adventofcode.solutions.Solution;
 import nl.underkoen.adventofcode.utils.InputUtils;
 
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
--- src/main/java/nl/underkoen/adventofcode/solutions/year2020/Day02.java
+++ src/main/java/nl/underkoen/adventofcode/solutions/year2020/Day02.java
@@ -2,8 +2,10 @@ package nl.underkoen.adventofcode.solutions.year2020;
 
 import lombok.Getter;
 import nl.underkoen.adventofcode.solutions.Solution;
+import nl.underkoen.adventofcode.utils.InputUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day02 extends Solution {
     @Getter private final int day = 2;
@@ -11,17 +13,25 @@ public class Day02 extends Solution {
 
     @Override
     public long[] getCorrectOutput() {
-        return new long[]{};
+        return new long[]{519, 708};
     }
-
-//    @Override
-//    public String[] getCorrectOutputText() {
-//        return new String[]{};
-//    }
 
     @Override
     protected void run(List<String> input) {
-        //TODO write implementation
+        InputUtils.asRegexGroupList(input, "(\\d+)-(\\d+) ([a-z]): ([a-z]+)")
+                .map(s -> s.collect(Collectors.toList()))
+                .forEach(l -> {
+                    long lower = Long.parseLong(l.get(0));
+                    long upper = Long.parseLong(l.get(1));
+                    char c = l.get(2).charAt(0);
+                    String text = l.get(3);
 
+                    long count = text.chars()
+                            .filter(v -> v == c)
+                            .count();
+
+                    if (count >= lower && count <= upper) a++;
+                    if ((text.charAt((int) lower - 1) == c) != (text.charAt((int) upper - 1) == c)) b++;
+                });
     }
 }
