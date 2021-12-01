0241985aa315898c0224985ffe7227205257dc5e
UnderKoen <koenvanstaveren@hotmail.com>
2021-12-01 07:54:05 +0100

    Day 1

--- src/main/java/nl/underkoen/adventofcode/solutions/year2021/Day01.java
+++ src/main/java/nl/underkoen/adventofcode/solutions/year2021/Day01.java
@@ -2,8 +2,11 @@ package nl.underkoen.adventofcode.solutions.year2021;
 
 import lombok.Getter;
 import nl.underkoen.adventofcode.solutions.Solution;
+import nl.underkoen.adventofcode.utils.InputUtils;
+import nl.underkoen.adventofcode.utils.StreamUtils;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Day01 extends Solution {
     @Getter private final int day = 1;
@@ -11,17 +14,20 @@ public class Day01 extends Solution {
 
     @Override
     public long[] getCorrectOutput() {
-        return new long[]{};
+        return new long[]{1529, 1567};
     }
-
-//    @Override
-//    public String[] getCorrectOutputText() {
-//        return new String[]{};
-//    }
 
     @Override
     protected void run(List<String> input) {
-        //TODO write implementation
+        List<Long> nums = InputUtils.asNumberList(input).toList();
 
+        a = StreamUtils.mapWithPrev(nums.stream(), (l1, l2) -> l1 < l2)
+                .filter(b -> b)
+                .count();
+
+        Stream<Long> window = StreamUtils.mapWithPrev(nums.stream(), (l1, l2, l3) -> l1 + l2 + l3);
+        b = StreamUtils.mapWithPrev(window, (l1, l2) -> l1 < l2)
+                .filter(b -> b)
+                .count();
     }
 }
---
+++ src/main/java/nl/underkoen/adventofcode/utils/StreamUtils.java
@@ -0,0 +1,70 @@
+package nl.underkoen.adventofcode.utils;
+
+import org.apache.commons.lang3.function.TriFunction;
+
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class StreamUtils {
+    public static <T, R> Stream<R> mapWithPrev(Stream<T> s, BiFunction<T, T, R> mapper) {
+        boolean parallel = s.isParallel();
+        Spliterator<T> spliterator = s.spliterator();
+
+        return StreamSupport.stream(new Spliterators.AbstractSpliterator<R>(
+                spliterator.estimateSize(),
+                spliterator.characteristics() & ~(Spliterator.SIZED | Spliterator.SUBSIZED)) {
+            Consumer<? super R> current;
+            T prev;
+            final Consumer<T> adapter = t -> {
+                if (prev != null) {
+                    current.accept(mapper.apply(prev, t));
+                }
+                prev = t;
+            };
+
+            @Override
+            public boolean tryAdvance(Consumer<? super R> action) {
+                current = action;
+                try {
+                    return spliterator.tryAdvance(adapter);
+                } finally {
+                    current = null;
+                }
+            }
+        }, parallel).onClose(s::close);
+    }
+
+    public static <T, R> Stream<R> mapWithPrev(Stream<T> s, TriFunction<T, T, T, R> mapper) {
+        boolean parallel = s.isParallel();
+        Spliterator<T> spliterator = s.spliterator();
+
+        return StreamSupport.stream(new Spliterators.AbstractSpliterator<R>(
+                spliterator.estimateSize(),
+                spliterator.characteristics() & ~(Spliterator.SIZED | Spliterator.SUBSIZED)) {
+            Consumer<? super R> current;
+            T prev1;
+            T prev2;
+            final Consumer<T> adapter = t -> {
+                if (prev1 != null && prev2 != null) {
+                    current.accept(mapper.apply(prev2, prev1, t));
+                }
+                prev2 = prev1;
+                prev1 = t;
+            };
+
+            @Override
+            public boolean tryAdvance(Consumer<? super R> action) {
+                current = action;
+                try {
+                    return spliterator.tryAdvance(adapter);
+                } finally {
+                    current = null;
+                }
+            }
+        }, parallel).onClose(s::close);
+    }
+}
